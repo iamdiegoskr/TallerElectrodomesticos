02ecbc406e6fcccf0e94242910adcea8fbee45a0
Diego Martinez <juandiego337@hotmail.com>
2021-09-02 21:38:45 -0500

    Implement switch in the section to create product

--- src/main/java/com/sofka/skr/Main.java
+++ src/main/java/com/sofka/skr/Main.java
@@ -10,9 +10,9 @@ public class Main {
 
     static ArrayList<HomeAppliances> listProducts = new ArrayList<>();
 
-    public static void main(String[] args) {
+    static Scanner lea = new Scanner(System.in);
 
-        Scanner lea = new Scanner(System.in);
+    public static void main(String[] args) {
 
         int option = 0;
 
@@ -26,62 +26,71 @@ public class Main {
                 System.out.println("Que tipo de electrodomestico ?  1.Televisor 2.Nevera 3.General");
                 int type = lea.nextInt();
 
-                if (type==1){
-
-                    System.out.println("Tipo de consumo del televisor? A,B,C" );
-                    String consumption = lea.next().toLowerCase();
-
-                    System.out.println("Ingrese el tipo de procedencia del tv? 1.Nacional 2.Importado");
-                    int origin = lea.nextInt();
-
-                    System.out.println("Ingrese el numero de pulgadas");
-                    int inches = lea.nextInt();
-
-                    System.out.println("El televisor tiene tdt ? 1.SI 2.NO");
-                    int haveTdt = lea.nextInt();
-
-                    Tv tv = new Tv(consumption.charAt(0), origin, inches, haveTdt);
-                    addProduct(tv);
-
-                    System.out.println(tv.getPriceTotal());
-
-
-                }else if(type==2){
-
-                    System.out.println("Tipo de consumo de la nevera ? A,B,C" );
-                    String consumption = lea.next().toLowerCase();
-
-                    System.out.println("Ingrese el tipo de procedencia de la nevera? 1.Nacional 2.Importado");
-                    int origin = lea.nextInt();
-
-                    System.out.println("Ingrese la capacidad de la nevera en litros : ");
-                    int capacity = lea.nextInt();
-
-                    Fridge fridge = new Fridge(consumption.charAt(0), origin, capacity);
-                    addProduct(fridge);
-
-                    System.out.println(fridge.getPriceTotal());
-                }else if(type==3){
-
-                    System.out.println("Tipo de consumo del electrodomestico? A,B,C" );
-                    String consumption = lea.next().toLowerCase();
-
-                    System.out.println("Ingrese el tipo de procedencia del electrodomestico? 1.Nacional 2.Importado");
-                    int origin = lea.nextInt();
-
-                    HomeAppliances homeAppliances = new HomeAppliances(consumption.charAt(0),origin);
-                    addProduct(homeAppliances);
-
-                    System.out.println(homeAppliances.getPriceTotal());
-
+                switch (type){
+                    case 1 :
+                        registerTv();
+                        break;
+                    case 2:
+                        registerFridge();
+                        break;
+                    case 3:
+                        registerHomeAppliance();
+                        break;
+                    default:
+                        System.out.println("Opcion invalida");
                 }
             }
-
         }while(option!=0);
-
         showListProducts();
+    }
 
+    public static void registerTv(){
 
+        System.out.println("Tipo de consumo del televisor? A,B,C" );
+        String consumption = lea.next().toLowerCase();
+
+        System.out.println("Ingrese el tipo de procedencia del tv? 1.Nacional 2.Importado");
+        int origin = lea.nextInt();
+
+        System.out.println("Ingrese el numero de pulgadas");
+        int inches = lea.nextInt();
+
+        System.out.println("El televisor tiene tdt ? 1.SI 2.NO");
+        int haveTdt = lea.nextInt();
+
+        Tv tv = new Tv(consumption.charAt(0), origin, inches, haveTdt);
+        addProduct(tv);
+
+        System.out.println(tv.getPriceTotal());
+    }
+
+    public static void registerFridge(){
+        System.out.println("Tipo de consumo de la nevera ? A,B,C" );
+        String consumption = lea.next().toLowerCase();
+
+        System.out.println("Ingrese el tipo de procedencia de la nevera? 1.Nacional 2.Importado");
+        int origin = lea.nextInt();
+
+        System.out.println("Ingrese la capacidad de la nevera en litros : ");
+        int capacity = lea.nextInt();
+
+        Fridge fridge = new Fridge(consumption.charAt(0), origin, capacity);
+        addProduct(fridge);
+
+        System.out.println(fridge.getPriceTotal());
+    }
+
+    public static void registerHomeAppliance(){
+        System.out.println("Tipo de consumo del electrodomestico? A,B,C" );
+        String consumption = lea.next().toLowerCase();
+
+        System.out.println("Ingrese el tipo de procedencia del electrodomestico? 1.Nacional 2.Importado");
+        int origin = lea.nextInt();
+
+        HomeAppliances homeAppliances = new HomeAppliances(consumption.charAt(0),origin);
+        addProduct(homeAppliances);
+
+        System.out.println(homeAppliances.getPriceTotal());
     }
 
     public static void addProduct(HomeAppliances product ){
